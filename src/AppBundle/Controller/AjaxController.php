31a252c1507973313ab40ad8e81beb3fcade8494
vtthuan <vtthuan89@gmail.com>
2017-03-28 02:37:55 +0200

    favoris

--- src/AppBundle/Controller/AjaxController.php
+++ src/AppBundle/Controller/AjaxController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Service\MotorSearch;
 use Application\Sonata\NewsBundle\Entity\Post;
 use AppBundle\Entity\Word;
+use AppBundle\Entity\Favorite;
 
 class AjaxController extends Controller
 {
@@ -53,7 +54,39 @@ class AjaxController extends Controller
        // }
     //return new Response('This is not ajax!', 400);
     }
-	
+
+    /**
+     * @Route("/addFavorite", name="add_favorite", options={"expose"=true})
+     * @Method("POST")
+     */
+    public function addFavoriteAction(Request $request){
+
+        echo "<script>javascript: alert('2')></script>";
+            $postId = $request->get('post');
+            $from = $request->get('from');
+            $to = $request->get('to');
+            $content = $request->get('content');
+
+            $post = $this->getDoctrine()
+                ->getRepository(Post::getEntityName())
+                ->findOneBy(array('id' => $postId));
+
+            $favorite = new Favorite();
+            $favorite->setPost($post);
+            $favorite->setEnd($to);
+            $favorite->setStart($from);
+            $favorite->setPhrase($content);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($favorite);
+            $em->flush();
+
+            $this->addFlash('success', 'video.updated_successfully');
+
+            return $this->render('post/translations/index.html.twig', array('translations' => ''));
+
+    }
+
 	function startsWith($haystack, $needle) {
 		// search backwards starting from haystack length characters from the end
 		return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
